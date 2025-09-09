3c07d357ee9f0cee2eca64b26a6eff8fa15432e2
NiKmAkMir <joraazot@gmail.com>
2025-09-09 13:26:26 +0300

    change links on site pages

--- first_task/views.py
+++ first_task/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index_page(request):
     return render(request, "index.html")
+
+def inner_page(request):
+    return render(request, "inner.html")
