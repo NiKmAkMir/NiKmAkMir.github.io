268424eb84b604e7079328a8c2a0798d2c2660bb
NiKmAkMir <joraazot@gmail.com>
2025-09-17 21:24:08 +0300

    del django

--- first_task/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index_page, name="index_page"),
-    path('inner_page', views.inner_page, name="inner_page")
-]
--- first_task/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index_page(request):
-    return render(request, "index.html")
-
-def inner_page(request):
-    return render(request, "inner.html")
