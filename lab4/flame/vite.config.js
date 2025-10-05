4d156d263ad4d934175f483aec02de7f409563d4
NiKmAkMir <joraazot@gmail.com>
2025-10-06 01:46:56 +0300

    fix vite

--- lab4/flame/vite.config.js
+++ lab4/flame/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lab4/',             
+  base: '/lab4/',              
   build: {
-    outDir: path.resolve(__dirname, '../../lab4/'), 
+    outDir: path.resolve(__dirname, '../../lab4/'),
     emptyOutDir: true
   }
 })
