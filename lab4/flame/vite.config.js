71e9952e3994514cea22fc99b0cb6b12a0d60e35
NiKmAkMir <joraazot@gmail.com>
2025-10-06 01:45:40 +0300

    deploy lab4

--- lab4/flame/vite.config.js
+++ lab4/flame/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lab4/'
+  base: '/lab4/',             
+  build: {
+    outDir: path.resolve(__dirname, '../../lab4/'), 
+    emptyOutDir: true
+  }
 })
