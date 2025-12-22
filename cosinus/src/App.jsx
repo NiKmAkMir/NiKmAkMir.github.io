5a1e8abd503df56414ae598d60f21a42b9fd8b3b
Асанов Богдан <asanovbogdan@Noutbuk-Asanov.local>
2025-12-22 15:38:55 +0300

    App.jsx

--- cosinus/src/App.jsx
+++ cosinus/src/App.jsx
@@ -1,4 +1,5 @@
-import Header from './components/Header';
+import React from 'react';
+import Header from './components/Header/Header';
 import HeroSection from './components/HeroSection';
 import ExperienceSection from './components/ExperienceSection';
 import SupportSection from './components/SupportSection';
